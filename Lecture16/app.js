2a7f0254bd0266aeb64e0d799fbaca7d0f6f3075
arnold <amwadwaa@kemri-wellcome.org>
2018-06-13 11:34:47 +0300

    Added Lecture15 and Lecture16 notes

---
+++ Lecture16/app.js
@@ -0,0 +1,28 @@
+(function(){
+    'use strict';
+    angular.module('CounterApp', [])
+    .controller('CounterController', CounterController);
+    
+    CounterController.$inject = ['$scope'];
+
+    function CounterController($scope) {
+        $scope.firstName = "Arnold";
+        $scope.fullName = "";
+
+        $scope.showNumberOfWatchers = function () {
+            console.log("# of Watchers: ", $scope.$$watchersCount);
+        };
+        
+        $scope.setFullName = function () {
+            $scope.fullName = $scope.firstName +  " " + "Mwadwaa";
+        };
+
+        $scope.logFirstName = function () {
+            console.log("First name is: ", $scope.firstName);
+        };
+
+        $scope.logFullName = function () {
+            console.log("Full name is: ", $scope.fullName);
+        };
+    }
+})();
